2db4ea71499c734e0dbe0b771ec6e14623c72e2c
KeoFoxy <arasdiel68@gmail.com>
2022-12-26 18:38:11 +0300

    get data by ip

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+const ipInput = document.querySelector('.search-bar__input');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', getData);
+ipInput.addEventListener('keyDown', handleKey)
+
+function getData(){
+    //check data
+    fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_5rjFguN0ChRp64Exf6qOO6VrvEw4E&ipAddress=${ipInput.value}`)
+        .then(response => response.json())
+        .then(console.log)
+}
+
+function handleKey(e){
+    if(e.key === 'Enter') {
+        getData();
+    }
+}
