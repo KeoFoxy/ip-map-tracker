263d9fd88a63a14b58cb6cc8e86d563745f1b43b
KeoFoxy <arasdiel68@gmail.com>
2022-12-26 19:11:16 +0300

    IP address info

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { validateIp } from "./helpers";
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('button');
 
+const ipInfo = document.querySelector('#ip');
+const locationInfo = document.querySelector('#location');
+const timezoneInfo = document.querySelector('#timezone');
+const ispInfo = document.querySelector('#isp');
+
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keyDown', handleKey)
 
@@ -10,7 +15,7 @@ function getData(){
     if(validateIp(ipInput.value)){
     fetch(`https://geo.ipify.org/api/v2/country?apiKey=at_5rjFguN0ChRp64Exf6qOO6VrvEw4E&ipAddress=${ipInput.value}`)
         .then(response => response.json())
-        .then(console.log)
+        .then(setInfo)
     }
 }
 
@@ -18,4 +23,12 @@ function handleKey(e){
     if(e.key === 'Enter') {
         getData();
     }
+}
+
+//202.11.23.11
+function setInfo(mapData){
+    ipInfo.innerText = mapData.ip;
+    locationInfo.innerText = mapData.location.country + ' ' + mapData.location.region;
+    timezoneInfo.innerText = mapData.location.timezone;
+    ispInfo.innerText = mapData.isp;
 }
